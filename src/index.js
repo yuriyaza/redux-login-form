c26ed73b32d17614f43cd866dcc85f768e4a816a
yuriyaza <yuriy.aza@gmail.com>
2023-07-09 22:15:06 +0300

    Update 2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/redux-login-form'>
+    <BrowserRouter basename='/redux-practice-login-form'>
       <Provider store={store}>
         <App />
       </Provider>
